d48e429422a266e5f99c63f72f06ad98404905fe
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-17 21:04:04 +0600

    create express app and install cors and mongodb

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.send("Crud is running on the site");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Crud is Running on port: ${port}`);
+  });
+  
+  
+  

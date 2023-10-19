4f2b7de481cf692dd932c2accd9dec7c98e1b2a5
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-19 10:40:06 +0600

    work on find data by brand

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
  
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3hdabzk.mongodb.net/?retryWrites=true&w=majority`;
-  console.log(uri);
+  
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
   const client = new MongoClient(uri, {
     serverApi: {
@@ -36,6 +36,12 @@ app.use(express.json())
       res.send(result)
     })
 
+      // find by brand 
+      app.get('/cars/:brand', async (req, res) => {
+        const brand = req.params.brand;
+        const result = await carsCollection.find({ brand: brand }).toArray();
+        res.send(result)
+      })
 
       // create 
       app.post('/cars', async (req, res) => {
